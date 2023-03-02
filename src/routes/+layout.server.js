7e0b141b9f6cb630acb63311f2a6ffc2ea167404
Ludovic Lahougue <a>
2023-03-02 13:46:41 +0100

    fix home

--- src/routes/+layout.server.js
+++ src/routes/+layout.server.js
@@ -3,10 +3,12 @@ import { getServerSession } from '@supabase/auth-helpers-sveltekit'
 export const load = async (event) => {
     const session = await getServerSession(event);
 
+    let username = null;
     const {
         data: { user },
     } = await event.locals.sb.auth.getUser()
-    const username = user.user_metadata.username
+    if(user != null)
+        username = user.user_metadata.username
     
     return {
         session: session,
