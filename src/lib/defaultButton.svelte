1c9bfb52e60671fabe4f4e440e65a2e0f53b0456
nbinet <nathan.binet99@gmail.com>
2023-03-02 19:15:49 +0100

    update defaultButton

--- src/lib/defaultButton.svelte
+++ src/lib/defaultButton.svelte
@@ -4,9 +4,11 @@
 </script>
 
 <button class="bg-orange rounded-xl" type="submit">
-	{#if link}
-		<a class="py-2 px-4 text-white" href={link}>{label}</a>
-	{:else}
-		<p class="py-2 px-4 text-white">{label}</p>
-	{/if}
+	<p class="py-2 px-4 text-white">
+		{#if link}
+			<a href={link}>{label}</a>
+		{:else}
+			{label}
+		{/if}
+	</p>
 </button>
