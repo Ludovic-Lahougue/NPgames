1e64bc3ac312a258b5b53b11aa6b7193443889ae
nbinet <nathan.binet99@gmail.com>
2023-03-02 16:16:04 +0100

    create footer component

---
+++ src/lib/footer.svelte
@@ -0,0 +1,13 @@
+<script>
+	const tab = [
+		{ label: "Scores", link: "", img: "" },
+		{ label: "Home", link: "", img: "" },
+		{ label: "Account", link: "", img: "" },
+	];
+</script>
+
+<div class="flex flex-row justify-evenly bg-indigo">
+	{#each tab as menu}
+		<a class="py-3 text-orange" href={menu.link}>{menu.label}</a>
+	{/each}
+</div>
