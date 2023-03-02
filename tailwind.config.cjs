1753529ca6420b26178a6740b565a87101fe4d83
nbinet <nathan.binet99@gmail.com>
2023-03-02 16:15:42 +0100

    add colors in tailwind.config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,6 +3,13 @@ module.exports = {
   content: ["./src/**/*.{svelte,html,js,ts}"],
   theme: {
     extend: {},
+    colors: {
+      orange: "#F1895C",
+      indigo: "#516079",
+      darkblue: "#2E3244",
+      grey: "#C5C6C6",
+      white: "#FFFFFF",
+    }
   },
   plugins: [],
 }
